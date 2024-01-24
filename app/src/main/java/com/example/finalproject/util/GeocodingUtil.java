d0e8824b455b04c203b0dc0773cacda1fd0f547b
Yair5675 <yairziv5675@gmail.com>
2024-01-24 13:01:28 +0200

    Added a utility function to get the bounds of a certain geocoding response. The function returns a Result in case the geocoding result doesn't contain bounds

--- app/src/main/java/com/example/finalproject/util/GeocodingUtil.java
+++ app/src/main/java/com/example/finalproject/util/GeocodingUtil.java
@@ -2,11 +2,13 @@ package com.example.finalproject.util;
 
 import android.util.Log;
 
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.maps.model.AddressComponent;
 import com.google.maps.model.AddressComponentType;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
 
 public class GeocodingUtil {
     public static void logResult(GeocodingResult result, String tag) {
@@ -52,5 +54,23 @@ public class GeocodingUtil {
             return Result.failure("Street number was not found in geocoding result");
         else
             return Result.success(route + " " + streetNumber);
+    }
+
+    public static Result<LatLngBounds, String> getBounds(GeocodingResult geocodingResult) {
+        // Get north-east and south-west borders:
+        final LatLng NE = geocodingResult.geometry.bounds.northeast;
+        final LatLng SW = geocodingResult.geometry.bounds.southwest;
+
+        // Make sure they're not null and then convert them to other google latitude-longitude
+        // objects:
+        if (NE == null || SW == null)
+            return Result.failure("Missing bounds for result");
+
+        final com.google.android.gms.maps.model.LatLng northeast =
+                new com.google.android.gms.maps.model.LatLng(NE.lat, NE.lng);
+        final com.google.android.gms.maps.model.LatLng southwest =
+                new com.google.android.gms.maps.model.LatLng(SW.lat, SW.lng);
+
+        return Result.success(new LatLngBounds(southwest, northeast));
     }
 }
