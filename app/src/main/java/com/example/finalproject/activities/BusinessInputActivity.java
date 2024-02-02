8f2d63bedeed65882900e741a7802931c11de219
Yair5675 <yairziv5675@gmail.com>
2024-02-02 18:56:53 +0200

    Added new activity called BusinessInputActivity that will register and update new businesses. Initial layout was created but will be changed later

---
+++ app/src/main/java/com/example/finalproject/activities/BusinessInputActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalproject.R;
+
+public class BusinessInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_input);
+    }
+}
