39ab1f3959d555be11a68f9cabd3eb35d547bee2
Yair5675 <yairziv5675@gmail.com>
2024-02-05 09:55:30 +0200

    Added validation that a branch with a similar company name doesn't already exists at this address

--- app/src/main/java/com/example/finalproject/fragments/input/business/BusinessRegistrationForm.java
+++ app/src/main/java/com/example/finalproject/fragments/input/business/BusinessRegistrationForm.java
@@ -3,10 +3,14 @@ package com.example.finalproject.fragments.input.business;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.finalproject.R;
 import com.example.finalproject.fragments.input.InputForm;
 import com.example.finalproject.util.Result;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.function.Consumer;
@@ -27,6 +31,12 @@ public class BusinessRegistrationForm extends InputForm {
     // The location of the branch:
     private String country, city, address;
 
+    // Tag for debugging purposes:
+    private static final String TAG = "BusinessRegistrationForm";
+
+    // Error message in case a similar branch was found:
+    private static final String SIMILAR_BRANCH_FOUND_ERROR = "Similar branch found";
+
     public BusinessRegistrationForm(Resources res, String userCountry) {
         super(
                 // Set the title:
@@ -43,8 +53,38 @@ public class BusinessRegistrationForm extends InputForm {
         // Load info from the fragments:
         this.loadFragmentsInfo();
 
-        // TODO: Check that a similar company doesn't already have a branch at that location:
+        // Check that a similar company doesn't already have a branch at that location:
+        this.validateSimilarBranch(unused -> {
+            // TODO: Add the new branch:
+        }, exception-> {
+            // Log the error and toast it:
+            Log.e(TAG, "Failed to validate branch", exception);
 
+            if (exception.getMessage() != null && exception.getMessage().equals(SIMILAR_BRANCH_FOUND_ERROR))
+                Toast.makeText(context, SIMILAR_BRANCH_FOUND_ERROR, Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void validateSimilarBranch(OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener) {
+        // Get a reference to firestore:
+        final FirebaseFirestore dbRef = FirebaseFirestore.getInstance();
+
+        // Check for similar branches:
+        final String fullAddress = String.format("%s %s %s", this.country, this.city, this.address);
+        dbRef.collection("branches")
+                .whereEqualTo("companyName", this.companyName)
+                .whereEqualTo("fullAddress", fullAddress)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    // Check that there is no similar branch:
+                    if (queryDocumentSnapshots.isEmpty())
+                        onSuccessListener.onSuccess(null);
+                    else
+                        onFailureListener.onFailure(new Exception(SIMILAR_BRANCH_FOUND_ERROR));
+                })
+                .addOnFailureListener(onFailureListener);
     }
 
     private void loadFragmentsInfo() {
