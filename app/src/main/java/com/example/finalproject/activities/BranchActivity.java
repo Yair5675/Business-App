26dd0685f234135e62c8196455d31e67929d3f6b
Yair5675 <yairziv5675@gmail.com>
2024-02-09 00:21:57 +0200

    Created the branch activity, an activity dedicated to show a specific branch and its employees. Later there will also be added a button to apply to the branch

---
+++ app/src/main/java/com/example/finalproject/activities/BranchActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalproject.R;
+
+public class BranchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branch);
+    }
+}
