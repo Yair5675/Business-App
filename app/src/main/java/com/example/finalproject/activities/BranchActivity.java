f046765a8155581871585a8cf8e728d8783286df
Yair5675 <yairziv5675@gmail.com>
2024-02-15 13:04:45 +0200

    Created the view pager in the branch activity that allows the managers to swipe between the employees fragment and the applications fragment

--- app/src/main/java/com/example/finalproject/activities/BranchActivity.java
+++ app/src/main/java/com/example/finalproject/activities/BranchActivity.java
@@ -2,15 +2,18 @@ package com.example.finalproject.activities;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.finalproject.R;
+import com.example.finalproject.custom_views.adapters.ScreenSlideAdapter;
 import com.example.finalproject.database.online.collections.Branch;
 import com.example.finalproject.database.online.collections.User;
+import com.example.finalproject.fragments.branch.ApplicationsFragment;
 import com.example.finalproject.fragments.branch.EmployeesFragment;
 
 import java.io.Serializable;
@@ -24,6 +27,12 @@ public class BranchActivity extends AppCompatActivity {
 
     // The employees fragment:
     private EmployeesFragment employeesFragment;
+
+    // The applications fragment:
+    private ApplicationsFragment applicationsFragment;
+
+    // The view pager that allows the user to swipe between fragments:
+    private ViewPager2 pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +51,27 @@ public class BranchActivity extends AppCompatActivity {
         // Create the employees fragment:
         this.employeesFragment = new EmployeesFragment(this.currentUser, this.currentBranch);
 
-        // Load the employees activity:
-        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.actBranchFragmentContainer, this.employeesFragment).commit();
+        // Create the applications fragment:
+        this.applicationsFragment = new ApplicationsFragment(this.currentBranch);
+
+        // Load the view pager:
+        this.pager = findViewById(R.id.actBranchPager);
+        this.initPagerAdapter();
 
         // Load the back button callback:
         this.loadBackButtonCallback();
 
+    }
+
+    private void initPagerAdapter() {
+        // Initialize the adapter and prevent the user from swiping at first:
+        ScreenSlideAdapter adapter = new ScreenSlideAdapter(this, this.getFragments());
+        this.pager.setAdapter(adapter);
+        this.pager.setCurrentItem(0);
+    }
+
+    private Fragment[] getFragments() {
+        return new Fragment[] { this.employeesFragment, this.applicationsFragment };
     }
 
     private void loadUserFromIntent() {
