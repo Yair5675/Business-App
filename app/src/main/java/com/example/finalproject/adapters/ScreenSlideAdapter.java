e83a0ca81dc92422c4993d4f40effa1ba2dd792f
Yair5675 <yairziv5675@gmail.com>
2024-02-18 14:01:19 +0200

    Created methods to add and remove a fragment

--- app/src/main/java/com/example/finalproject/adapters/ScreenSlideAdapter.java
+++ app/src/main/java/com/example/finalproject/adapters/ScreenSlideAdapter.java
@@ -7,7 +7,6 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ScreenSlideAdapter extends FragmentStateAdapter {
@@ -28,5 +27,29 @@ public class ScreenSlideAdapter extends FragmentStateAdapter {
     @Override
     public int getItemCount() {
         return this.fragments.size();
+    }
+
+    /**
+     * Adds a fragment to the end of the fragments list.
+     * @param fragment A fragment that will be added to the adapter last.
+     */
+    public void addFragment(Fragment fragment) {
+        this.fragments.add(fragment);
+        this.notifyItemInserted(this.fragments.size() - 1);
+    }
+
+    /**
+     * Removes the given fragment from the adapter.
+     * @param fragment A fragment that will be removed from the adapter.
+     */
+    public void removeFragment(Fragment fragment) {
+        // Get the index of the fragment:
+        final int index = this.fragments.indexOf(fragment);
+
+        // Remove the fragment:
+        if (index != -1) {
+            this.fragments.remove(index);
+            this.notifyItemRemoved(index);
+        }
     }
 }
