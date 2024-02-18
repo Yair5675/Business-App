995fd7219573b56531ab0a41716e7d92ce2fb098
Yair5675 <yairziv5675@gmail.com>
2024-02-18 13:53:07 +0200

    Replaces the type of the fragments attribute from a regular array to a List<Fragment> (ArrayList in that case) to support adding and removing fragments

--- app/src/main/java/com/example/finalproject/adapters/ScreenSlideAdapter.java
+++ app/src/main/java/com/example/finalproject/adapters/ScreenSlideAdapter.java
@@ -5,23 +5,28 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ScreenSlideAdapter extends FragmentStateAdapter {
     // The fragments inside the main activity:
-    private final Fragment[] fragments;
+    private final List<Fragment> fragments;
 
-    public ScreenSlideAdapter(@NonNull FragmentActivity fragmentActivity, Fragment[] fragments) {
+    public ScreenSlideAdapter(@NonNull FragmentActivity fragmentActivity, Fragment ... fragments) {
         super(fragmentActivity);
-        this.fragments = fragments;
+        this.fragments = new ArrayList<>(Arrays.asList(fragments));
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return this.fragments[position];
+        return this.fragments.get(position);
     }
 
     @Override
     public int getItemCount() {
-        return this.fragments.length;
+        return this.fragments.size();
     }
 }
