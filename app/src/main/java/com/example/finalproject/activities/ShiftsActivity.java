b12301cc9f42e3490c3b32cb8910a7a00cb7a160
Yair5675 <yairziv5675@gmail.com>
2024-02-21 10:53:50 +0200

    Saved info about the shift using the UserShift class in the users collection

--- app/src/main/java/com/example/finalproject/activities/ShiftsActivity.java
+++ app/src/main/java/com/example/finalproject/activities/ShiftsActivity.java
@@ -18,6 +18,7 @@ import com.example.finalproject.custom_views.ShiftView;
 import com.example.finalproject.database.online.collections.Branch;
 import com.example.finalproject.database.online.collections.Employee;
 import com.example.finalproject.database.online.collections.Shift;
+import com.example.finalproject.database.online.collections.UserShift;
 import com.example.finalproject.database.online.collections.Worker;
 import com.example.finalproject.fragments.shifts.DayShiftsFragment;
 import com.example.finalproject.util.Util;
@@ -325,10 +326,16 @@ public class ShiftsActivity extends AppCompatActivity implements TabLayout.OnTab
             batch.set(workerRef, worker, SetOptions.merge());
 
             // Set the shift for the worker:
+            final UserShift userShift = new UserShift();
+            userShift.setUid(worker.getUid());
+            userShift.setDate(shift.getDate());
+            userShift.setStartingTime(shift.getStartingTime());
+            userShift.setEndingTime(shift.getEndingTime());
+            userShift.setRoleName(worker.getRoleName());
             DocumentReference shiftNotificationRef = this.db.document(String.format(
                     "users/%s/workplaces/%s/user_shifts/%s", worker.getUid(), this.branch.getBranchId(), shift.getShiftId()
             ));
-            batch.set(shiftNotificationRef, shift, SetOptions.merge());
+            batch.set(shiftNotificationRef, userShift, SetOptions.merge());
         }
 
         // Commit the branch:
