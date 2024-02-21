43c07563088f8874b324f9c247ba8105f5126a4a
Yair5675 <yairziv5675@gmail.com>
2024-02-21 11:09:21 +0200

    Changed the generic type of the class from Shift to UserShift

--- app/src/main/java/com/example/finalproject/adapters/OnlineUserShiftsAdapter.java
+++ app/src/main/java/com/example/finalproject/adapters/OnlineUserShiftsAdapter.java
@@ -10,49 +10,58 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalproject.R;
-import com.example.finalproject.database.online.collections.Shift;
+import com.example.finalproject.database.online.collections.UserShift;
 import com.example.finalproject.util.Constants;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 import java.util.Locale;
 
-public class OnlineUserShiftsAdapter extends OnlineAdapter<Shift, OnlineUserShiftsAdapter.ShiftVH> {
+public class OnlineUserShiftsAdapter extends OnlineAdapter<UserShift, OnlineUserShiftsAdapter.UserShiftVH> {
 
-    public OnlineUserShiftsAdapter(Context context, Runnable onEmptyCallback, Runnable onNotEmptyCallback, @NonNull FirestoreRecyclerOptions<Shift> options) {
+
+    public OnlineUserShiftsAdapter(Context context, Runnable onEmptyCallback, Runnable onNotEmptyCallback, @NonNull FirestoreRecyclerOptions<UserShift> options) {
         super(context, onEmptyCallback, onNotEmptyCallback, options);
-    }
-
-    @Override
-    protected void onBindViewHolder(@NonNull OnlineUserShiftsAdapter.ShiftVH holder, int position, @NonNull Shift shift) {
-        // Load the details into the text views:
-        holder.tvCompanyName.setText(shift.getCompanyName());
-
-        holder.tvDate.setText(String.format(
-                Locale.getDefault(), "At: %s", Constants.DATE_FORMAT.format(shift.getDate())
-        ));
-
-        holder.tvStartTime.setText(String.format(
-                Locale.getDefault(), "From: %d:%d", shift.getStartingTime() / 60, shift.getStartingTime() % 60
-        ));
-
-        holder.tvEndTime.setText(String.format(
-                Locale.getDefault(), "To: %d:%d", shift.getEndingTime() / 60, shift.getEndingTime() % 60
-        ));
     }
 
     @NonNull
     @Override
-    public OnlineUserShiftsAdapter.ShiftVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public OnlineUserShiftsAdapter.UserShiftVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Inflate the xml file and create a custom view holder with it:
         final View rowView = LayoutInflater.from(this.context).inflate(R.layout.row_user_shift, parent, false);
-        return new ShiftVH(rowView);
+        return new UserShiftVH(rowView);
     }
 
-    public static class ShiftVH extends RecyclerView.ViewHolder {
+    @Override
+    protected void onBindViewHolder(@NonNull UserShiftVH holder, int position, @NonNull UserShift shift) {
+        // Set the company name and role:
+        holder.tvCompanyName.setText(String.format(
+                Locale.getDefault(), "%s - %s", shift.getCompanyName(), shift.getRoleName()
+        ));
+
+        // Set the date:
+        holder.tvDate.setText(String.format(
+                Locale.getDefault(),
+                "At: %s", Constants.DATE_FORMAT.format(shift.getDate())
+        ));
+
+        // Set starting time:
+        holder.tvStartTime.setText(String.format(
+                Locale.getDefault(), "From: %d:%d",
+                shift.getStartingTime() / 60, shift.getStartingTime() % 60
+        ));
+
+        // Set ending time:
+        holder.tvEndTime.setText(String.format(
+                Locale.getDefault(), "To: %d:%d",
+                shift.getEndingTime() / 60, shift.getEndingTime() % 60
+        ));
+    }
+
+    public static class UserShiftVH extends RecyclerView.ViewHolder {
         // The text views of the row:
         private final TextView tvCompanyName, tvDate, tvStartTime, tvEndTime;
 
-        public ShiftVH(@NonNull View itemView) {
+        public UserShiftVH(@NonNull View itemView) {
             super(itemView);
 
             // Load the views:
