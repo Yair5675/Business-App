e03eed7b2f8e9a30999f3e3d61e782a4d045ea4d
Yair5675 <yairziv5675@gmail.com>
2024-02-21 10:50:12 +0200

    Created a class for representing the information that is saved for the user about the shifts

---
+++ app/src/main/java/com/example/finalproject/database/online/collections/UserShift.java
@@ -0,0 +1,61 @@
+package com.example.finalproject.database.online.collections;
+
+import java.util.Date;
+
+public class UserShift {
+    // The user's ID:
+    private String uid;
+
+    // The role of the user:
+    private String roleName;
+
+    // The date of the shift:
+    private Date date;
+
+    // The starting and ending time of the shift:
+    private int startingTime, endingTime;
+
+    public UserShift() {
+        // Public empty constructor for firestore
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getStartingTime() {
+        return startingTime;
+    }
+
+    public void setStartingTime(int startingTime) {
+        this.startingTime = startingTime;
+    }
+
+    public int getEndingTime() {
+        return endingTime;
+    }
+
+    public void setEndingTime(int endingTime) {
+        this.endingTime = endingTime;
+    }
+}
