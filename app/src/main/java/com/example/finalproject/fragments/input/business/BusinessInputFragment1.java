d3cb62f3b2bd2839e5092249a30b6ca691feef83
Yair5675 <yairziv5675@gmail.com>
2024-02-25 10:34:54 +0200

    Removed to do I had not previously removed

--- app/src/main/java/com/example/finalproject/fragments/input/business/BusinessInputFragment1.java
+++ app/src/main/java/com/example/finalproject/fragments/input/business/BusinessInputFragment1.java
@@ -185,7 +185,6 @@ public class BusinessInputFragment1 extends InputFragment {
     }
 
     private void addValidationFunctions() {
-        // TODO: Create a custom validation function for the company name
         // Add validation functions for all inputs:
         this.validationFunctions.put(this.etCompanyName, InputValidation::validateCompanyName);
         this.validationFunctions.put(this.etBranchPassword, InputValidation::validatePassword);
