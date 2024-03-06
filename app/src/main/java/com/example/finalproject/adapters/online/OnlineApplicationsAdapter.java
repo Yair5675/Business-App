f33d084c5f7da4e1b5cad02db544737659d134fe
Yair5675 <yairziv5675@gmail.com>
2024-03-06 13:03:36 +0200

    Instead of using a cloud function, the acceptation/rejection of an application is done directly in the app. Furthermore, it now also decreases the amount of pending applications in the branch.

--- app/src/main/java/com/example/finalproject/adapters/online/OnlineApplicationsAdapter.java
+++ app/src/main/java/com/example/finalproject/adapters/online/OnlineApplicationsAdapter.java
@@ -15,20 +15,31 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalproject.R;
-import com.example.finalproject.database.online.CloudFunctionsHandler;
 import com.example.finalproject.database.online.StorageUtil;
 import com.example.finalproject.database.online.collections.Application;
 import com.example.finalproject.database.online.collections.Branch;
+import com.example.finalproject.database.online.collections.Employee;
+import com.example.finalproject.database.online.collections.User;
+import com.example.finalproject.database.online.collections.Workplace;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Transaction;
 
 import java.util.Locale;
 
 public class OnlineApplicationsAdapter extends OnlineAdapter<Application, OnlineApplicationsAdapter.ApplicationVH> {
+    // A reference to the online database:
+    private final FirebaseFirestore db;
+
     // The current branch that the applications are referring to:
     private Branch currentBranch;
-
-    // A reference to the cloud functions handler:
-    private final CloudFunctionsHandler functionsHandler;
 
     // Tag for debugging purposes:
     private static final String TAG = "OnlineApplicationsAdapter";
@@ -38,8 +49,8 @@ public class OnlineApplicationsAdapter extends OnlineAdapter<Application, Online
             @NonNull FirestoreRecyclerOptions<Application> options
     ) {
         super(context, onEmptyCallback, onNotEmptyCallback, options);
+        this.db = FirebaseFirestore.getInstance();
         this.currentBranch = currentBranch;
-        this.functionsHandler = CloudFunctionsHandler.getInstance();
     }
 
     public void setCurrentBranch(Branch currentBranch) {
@@ -75,43 +86,128 @@ public class OnlineApplicationsAdapter extends OnlineAdapter<Application, Online
         // Show the progress bar:
         holder.pbLoading.setVisibility(View.VISIBLE);
 
-        // Call the cloud function to accept or reject the user:
-        this.functionsHandler.resolveApplication(
-                application.getUid(),
-                this.currentBranch,
-                // TODO: When rejecting/accepting, decrement the pending applications in the branch
-                accepted,
-                false, // TODO: Change that later
-                () -> {
-                    // Show the two buttons:
-                    holder.btnAccept.setVisibility(View.VISIBLE);
-                    holder.btnReject.setVisibility(View.VISIBLE);
+        // Define callbacks:
+        final OnSuccessListener<Void> onSuccessListener = unused -> {
+            // Show the two buttons:
+            holder.btnAccept.setVisibility(View.VISIBLE);
+            holder.btnReject.setVisibility(View.VISIBLE);
 
-                    // Hide the progress bar:
-                    holder.pbLoading.setVisibility(View.GONE);
+            // Hide the progress bar:
+            holder.pbLoading.setVisibility(View.GONE);
 
-                    // Alert the user:
-                    final String msg = String.format(
-                            Locale.getDefault(),
-                            "%s was successfully %s",
-                            application.getUserFullName(), accepted ? "accepted" : "rejected"
-                    );
-                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-                }, e -> {
-                    // Show the two buttons:
-                    holder.btnAccept.setVisibility(View.VISIBLE);
-                    holder.btnReject.setVisibility(View.VISIBLE);
+            // Alert the user:
+            final String msg = String.format(
+                    Locale.getDefault(),
+                    "%s was successfully %s",
+                    application.getUserFullName(), accepted ? "accepted" : "rejected"
+            );
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        };
+        final OnFailureListener onFailureListener = e -> {
+            // Show the two buttons:
+            holder.btnAccept.setVisibility(View.VISIBLE);
+            holder.btnReject.setVisibility(View.VISIBLE);
 
-                    // Hide the progress bar:
-                    holder.pbLoading.setVisibility(View.GONE);
+            // Hide the progress bar:
+            holder.pbLoading.setVisibility(View.GONE);
 
-                    // Alert the user:
-                    Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+            // Alert the user:
+            Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
 
-                    // Log the error:
-                    Log.e(TAG, "Failed to accept/reject user", e);
-                }
-        );
+            // Log the error:
+            Log.e(TAG, "Failed to accept/reject user", e);
+        };
+
+        // Accept or reject the application:
+        if (accepted)
+            this.acceptApplication(application.getUid(), application.getUid(), onSuccessListener, onFailureListener);
+        else
+            this.rejectApplication(application.getUid(), onSuccessListener, onFailureListener);
+    }
+
+    private void acceptApplication(
+            String applicationId, String uid,
+            OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener
+    ) {
+        // Create the document references:
+        final DocumentReference applicationRef = this.db.document(String.format(
+                "branches/%s/applications/%s", this.currentBranch.getBranchId(), applicationId
+        )), workplaceRef = this.db.document(String.format(
+                "users/%s/workplaces/%s", uid, this.currentBranch.getBranchId()
+        )), employeeRef = this.db.document(String.format(
+                "branches/%s/employees/%s", this.currentBranch.getBranchId(), uid
+        )), userRef = this.db.collection("users").document(uid);
+
+        // Create the workplace object (the manager is not a manager):
+        final Workplace workplace = Workplace.fromBranch(this.currentBranch, false);
+
+        // Run the transaction:
+        this.db.runTransaction((Transaction.Function<Void>) transaction -> {
+            // Get the application and user document. This ensures it is not changed from
+            // another device during the transaction:
+            final DocumentSnapshot applicationDoc = transaction.get(applicationRef);
+            final DocumentSnapshot userDoc = transaction.get(userRef);
+            final DocumentSnapshot branchDoc = transaction.get(this.currentBranch.getReference());
+
+            if (!applicationDoc.exists())
+                throw new FirebaseFirestoreException("Application document does not exist", FirebaseFirestoreException.Code.NOT_FOUND);
+            if (!userDoc.exists())
+                throw new FirebaseFirestoreException("User document does not exist", FirebaseFirestoreException.Code.NOT_FOUND);
+
+            final User user = userDoc.toObject(User.class);
+            if (user == null)
+                throw new FirebaseFirestoreException("Couldn't convert user document to object", FirebaseFirestoreException.Code.INVALID_ARGUMENT);
+
+            if (!branchDoc.exists())
+                throw new FirebaseFirestoreException("Branch document does not exist", FirebaseFirestoreException.Code.NOT_FOUND);
+
+            // Set the workplace and employee documents:
+            transaction.set(workplaceRef, workplace, SetOptions.merge());
+            transaction.set(employeeRef, Employee.fromUser(user, false), SetOptions.merge());
+
+            // Lower the amount of pending applications in the branch (if it's above 0):
+            final Long pending = branchDoc.getLong(Branch.PENDING_APPLICATIONS);
+            if (pending == null)
+                throw new FirebaseFirestoreException("Document doesn't contain pending applications", FirebaseFirestoreException.Code.INVALID_ARGUMENT);
+
+            if (pending > 0)
+                transaction.update(this.currentBranch.getReference(), Branch.PENDING_APPLICATIONS, FieldValue.increment(-1));
+
+            // Delete the application:
+            transaction.delete(applicationRef);
+
+            return null;
+        }).addOnSuccessListener(onSuccessListener).addOnFailureListener(onFailureListener);
+    }
+
+    private void rejectApplication(
+            String applicationId, OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener
+    ) {
+        // Get references:
+        final DocumentReference applicationRef = this.db.document(String.format(
+                "branches/%s/applications/%s", this.currentBranch.getBranchId(), applicationId
+        )), branchRef = this.currentBranch.getReference();
+
+        // Make a transaction:
+        this.db.runTransaction((Transaction.Function<Void>) transaction -> {
+            // Get the branch:
+            final DocumentSnapshot branchDoc = transaction.get(branchRef);
+
+            if (!branchDoc.exists())
+                throw new FirebaseFirestoreException("Branch document does not exist", FirebaseFirestoreException.Code.NOT_FOUND);
+
+            // Lower the amount of applications if it's above 0:
+            final Long pending = branchDoc.getLong(Branch.PENDING_APPLICATIONS);
+            if (pending == null)
+                throw new FirebaseFirestoreException("Document doesn't contain pending applications", FirebaseFirestoreException.Code.INVALID_ARGUMENT);
+            if (pending > 0)
+                transaction.update(branchRef, Branch.PENDING_APPLICATIONS, FieldValue.increment(-1));
+
+            // Delete the application:
+            transaction.delete(applicationRef);
+
+            return null;
+        }).addOnSuccessListener(onSuccessListener).addOnFailureListener(onFailureListener);
     }
 
     public static class ApplicationVH extends RecyclerView.ViewHolder {
