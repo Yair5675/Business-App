6a6cca28912106c3b522b4306e479dfddc1ff4d0
Yair5675 <yairziv5675@gmail.com>
2024-03-07 19:22:15 +0200

    Removed unused methods

--- app/src/main/java/com/example/finalproject/util/Util.java
+++ app/src/main/java/com/example/finalproject/util/Util.java
@@ -2,29 +2,22 @@ package com.example.finalproject.util;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.os.Environment;
 import android.text.Editable;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.Timestamp;
 
 import org.jetbrains.annotations.Nullable;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
@@ -89,59 +82,6 @@ public class Util {
         imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
     }
 
-    /**
-     * Opens an image file from the external files directory and returns it in Bitmap format.
-     * @param context Context that is required when accessing the external files directory.
-     * @param imageName The name of the image file in question.
-     * @return If the file exists, it is returned in bitmap format wrapped with the Result class.
-     *         If not, a FileNotFoundException is returned wrapped with the Result class instead.
-     */
-    public static Result<Bitmap, FileNotFoundException> getImage(Context context, String imageName)
-    {
-        try {
-            // Open the PNG file using FileInputStream:
-            final File externalStorage = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            final File dir = new File(externalStorage, Constants.IMAGES_FOLDER_NAME);
-            final File dest = new File(dir, imageName);
-
-            FileInputStream fileInputStream = new FileInputStream(dest);
-
-            // Decode the input stream into a Bitmap:
-            return Result.success(BitmapFactory.decodeStream(fileInputStream));
-
-        } catch (FileNotFoundException e) {
-            return Result.failure(e);
-        }
-    }
-
-    /**
-     * Deletes an image from the external files directory.
-     * @param context Context that is required when accessing the external files directory.
-     * @param imageName The name of the image file which will be deleted.
-     * @return If the operation went well, an empty Result will be returned. Otherwise, a Result
-     *         with an error message will be returned.
-     */
-    public static Result<Void, String> deleteImage(Context context, String imageName) {
-        // Get the picture in storage:
-        final File externalStorage = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        final File dir = new File(externalStorage, Constants.IMAGES_FOLDER_NAME);
-
-        // Make sure the directory exists:
-        if (!dir.exists())
-            return Result.failure("External storage dir doesn't exist");
-
-        // Make sure the file itself exists:
-        final File pic = new File(dir, imageName);
-        if (!pic.exists())
-            return Result.failure("Profile pic file doesn't exist");
-
-        // Delete the file:
-        if (pic.delete())
-            return Result.success(null);
-        else
-            return Result.failure("Error occurred while deleting profile pic file");
-    }
-
     public static String fixNamingCapitalization(String name) {
         if (name == null || name.isEmpty())
             return "";
@@ -155,11 +95,6 @@ public class Util {
             }
         }
         return String.join(" ", words);
-    }
-
-    public static void setCircularImage(Context context, ImageView imageHolder, @DrawableRes int id) {
-        final RequestOptions ro = new RequestOptions().circleCrop();
-        Glide.with(context).load(id).apply(ro).into(imageHolder);
     }
 
     public static void setCircularImage(Context context, ImageView imageHolder, Bitmap image) {
