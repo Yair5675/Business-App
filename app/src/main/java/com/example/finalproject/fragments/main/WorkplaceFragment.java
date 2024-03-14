f3e2054b25c70472fb6ba2d5dafd848ba23caa41
Yair5675 <yairziv5675@gmail.com>
2024-03-14 11:25:45 +0200

    Created new fragment whose purpose is to show the user their workplaces, including old workplaces. The fragment isn't finished, only its XML design

---
+++ app/src/main/java/com/example/finalproject/fragments/main/WorkplaceFragment.java
@@ -0,0 +1,11 @@
+package com.example.finalproject.fragments.main;
+
+import androidx.fragment.app.Fragment;
+
+public class WorkplaceFragment extends Fragment {
+    public WorkplaceFragment() {}
+
+    public static WorkplaceFragment newInstance() {
+        return null;
+    }
+}
