bdd5dff0fc3e764a26e65bb6418a2b47ab8e029c
Yair5675 <yairziv5675@gmail.com>
2024-03-15 17:06:15 +0200

    Removed the ShiftsHandler.java class. This class was unused and not needed for the shifts

--- app/src/main/java/com/example/finalproject/database/online/ShiftsHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject.database.online;
-
-public class ShiftsHandler {
-    // TODO: After changing the shifts collection, check if this class is needed. If it is,
-    //  implement necessary shift loading methods and pass it to the shifts activity when entering
-    //  it
-
-}
