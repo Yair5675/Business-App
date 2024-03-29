c3d5be28f890376201accbe5b67dfccbb640699f
Yair5675 <yairziv5675@gmail.com>
2024-03-29 15:34:53 +0300

    Deleted unused cloud functions

--- app/src/main/java/com/example/finalproject/database/online/CloudFunctionsHandler.java
+++ app/src/main/java/com/example/finalproject/database/online/CloudFunctionsHandler.java
@@ -1,12 +1,8 @@
 package com.example.finalproject.database.online;
 
-import com.example.finalproject.database.online.collections.Branch;
-import com.example.finalproject.database.online.collections.User;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.functions.FirebaseFunctions;
 
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -77,89 +73,4 @@ public class CloudFunctionsHandler {
                 .addOnFailureListener(onFailureListener);
     }
 
-    public void deleteBranch(
-            String branchId,
-            Runnable onSuccessListener,
-            OnFailureListener onFailureListener
-    ) {
-        // Call the function with the branch ID:
-        final Map<String, String> data = new HashMap<>();
-        data.put("branchId", branchId);
-        this.functions
-                .getHttpsCallable("delete_branch")
-                .call(data)
-                .addOnSuccessListener(_r -> onSuccessListener.run())
-                .addOnFailureListener(onFailureListener);
-    }
-
-    public void deleteAllShifts(
-            LocalDate date,
-            String branchId,
-            Runnable onSuccessListener,
-            OnFailureListener onFailureListener
-    ) {
-        // Load the parameters:
-        final Map<String, Object> data = new HashMap<>();
-        data.put("day", date.getDayOfMonth());
-        data.put("month", date.getMonthValue());
-        data.put("year", date.getYear());
-        data.put("branchId", branchId);
-
-        // Call the function:
-        this.functions
-                .getHttpsCallable("delete_all_shifts")
-                .call(data)
-                .addOnSuccessListener(_r -> onSuccessListener.run())
-                .addOnFailureListener(onFailureListener);
-    }
-
-    public void resolveApplication(
-            String uid,
-            Branch branch,
-            boolean accepted,
-            boolean isManager,
-            Runnable onSuccessListener,
-            OnFailureListener onFailureListener
-    ) {
-        // Get the user from the database:
-        final FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users")
-                .document(uid)
-                .get()
-                .addOnSuccessListener(documentSnapshot -> {
-                    // Convert the document to a user object:
-                    final User user = documentSnapshot.toObject(User.class);
-                    if (user == null) {
-                        onFailureListener.onFailure(new Exception("Couldn't load user"));
-                        return;
-                    }
-
-                    if (accepted) {
-                        // Jsonify parameters and send to the cloud function:
-                        final Map<String, Object> data = new HashMap<>();
-                        data.put("user", user.jsonifyUser());
-                        data.put("branch", branch.jsonifyBranch());
-                        data.put("manager", isManager);
-
-                        this.functions
-                                .getHttpsCallable("accept_application")
-                                .call(data)
-                                .addOnSuccessListener(_r -> onSuccessListener.run())
-                                .addOnFailureListener(onFailureListener);
-                    }
-                    else {
-                        // If they are not accepted, just delete the application from the database:
-                        db
-                                .collection("branches")
-                                .document(branch.getBranchId())
-                                .collection("applications")
-                                .document(uid)
-                                .delete()
-                                .addOnSuccessListener(_r -> onSuccessListener.run())
-                                .addOnFailureListener(onFailureListener);
-                    }
-
-                })
-                .addOnFailureListener(onFailureListener);
-    }
 }
