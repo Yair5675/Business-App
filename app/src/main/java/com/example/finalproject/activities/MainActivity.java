7a9db43822e759e5aa26956c79c15647fa584fe3
Yair5675 <yairziv5675@gmail.com>
2024-03-29 18:19:31 +0300

    Made the app ask for SMS permission in the main activity

--- app/src/main/java/com/example/finalproject/activities/MainActivity.java
+++ app/src/main/java/com/example/finalproject/activities/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
@@ -32,6 +33,7 @@ import com.example.finalproject.fragments.main.PersonalFragment;
 import com.example.finalproject.fragments.input.user.UserRegistrationForm;
 import com.example.finalproject.fragments.main.ShiftsFragment;
 import com.example.finalproject.fragments.main.WorkplaceFragment;
+import com.example.finalproject.util.Permissions;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -77,6 +79,10 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ask the user for permission to send messages (necessary for other parts of the app):
+        if (!Permissions.checkPermissions(this, Manifest.permission.SEND_SMS))
+            Permissions.requestPermissions(this, Manifest.permission.SEND_SMS);
 
         // Initialize the online database reference:
         this.db = OnlineDatabase.getInstance();
